235b49f3e4d654b3265f9df22ca6dae9e2cfcc09
Vinay DG <vinay.dg@quation.in>
2026-08-26 17:01:53 +0530

    update the policy for github role

--- openbao/tofu/modules/openbao-github-core/main.tf
+++ openbao/tofu/modules/openbao-github-core/main.tf
@@ -1,6 +1,5 @@
 # Enable JWT Authentication Backend for GitHub Actions OIDC (Once per namespace)
 resource "vault_jwt_auth_backend" "github" {
-  namespace = var.namespace_path
   path               = "github"
   type               = "jwt"
   oidc_discovery_url = "https://token.actions.githubusercontent.com"
@@ -9,34 +8,25 @@ resource "vault_jwt_auth_backend" "github" {
 
 # Policy allowing complete configuration management inside the namespace
 resource "vault_policy" "tofu_management" {
-  namespace = var.namespace_path
   name      = "opentofu-management"
   policy    = <<EOT
-path "metadata/*" {
-  capabilities = ["create", "read", "update", "delete", "list"]
-}
-path "data/*" {
-  capabilities = ["create", "read", "update", "delete", "list"]
-}
-path "sys/*" {
-  capabilities = ["create", "read", "update", "delete", "list", "sudo"]
-}
-path "transit/*" {
-  capabilities = ["create", "read", "update", "delete", "list", "sudo"]
-}
-path "transit/datakey/plaintext/*" {
-  capabilities = ["create", "read", "update", "delete", "list", "sudo"]
-}
-path "auth/${vault_jwt_auth_backend.github.path}/*" {
-  capabilities = ["create", "read", "update", "delete", "list", "sudo"]
-}
+  path "sys/namespaces/${var.namespace_path}" {
+    capabilities = ["create", "read", "update", "delete", "list", "sudo"]
+  }
+  
+  # Allow full control over anything inside the namespace
+  path "sys/namespaces/${var.namespace_path}/*" {
+    capabilities = ["create", "read", "update", "delete", "list", "sudo"]
+  }
+  path "auth/${vault_jwt_auth_backend.github.path}/*" {
+    capabilities = ["create", "read", "update", "delete", "list", "sudo"]
+  }
 
 EOT
 }
 
 # Map GitHub OIDC claims to the Management Policy
 resource "vault_jwt_auth_backend_role" "tofu_management" {
-  namespace = var.namespace_path
   backend        = vault_jwt_auth_backend.github.path
   role_name      = "opentofu-manager"
   token_policies = [vault_policy.tofu_management.name, "opentofu-state-encryption"]

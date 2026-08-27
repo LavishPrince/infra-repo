19a6dbfe1777bbf67cefdc07ca89936eeadbf1f7
Vinay DG <vinay.dg@quation.in>
2026-08-27 13:57:36 +0530

    add a new branch for web app

--- openbao/tofu/kv-store.tf
+++ openbao/tofu/kv-store.tf
@@ -4,7 +4,7 @@ module "project_kv_store" {
   mount_path     = "project-secrets"
 
   project_branches = {
-    web-app     = ["main", "staging"]
+    web-app     = ["main", "staging", "dev"]
     api-service = ["main", "dev"]
   }
 }
